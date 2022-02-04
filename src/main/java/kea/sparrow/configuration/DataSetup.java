9c7b90756e67307628dc92678a5e7c98ac0308b9
bart7711 <bart0594@stud.kea.dk>
2022-02-04 01:29:35 +0100

    added DataSetup

--- src/main/java/kea/sparrow/configuration/DataSetup.java
+++ src/main/java/kea/sparrow/configuration/DataSetup.java
@@ -1,6 +1,13 @@
 package kea.sparrow.configuration;
 
 
+import kea.sparrow.entities.Post;
+import kea.sparrow.entities.User;
+import kea.sparrow.repositories.PostRepository;
+import kea.sparrow.repositories.UserRepository;
+import kea.sparrow.security.entities.ERole;
+import kea.sparrow.security.entities.Role;
+import kea.sparrow.security.repositories.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,9 +19,46 @@ import org.springframework.stereotype.Component;
 @Profile("!test")
 public class DataSetup implements CommandLineRunner {
 
+    PostRepository postRepo;
+    UserRepository userRepo;
+    RoleRepository roleRepository;
+    UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
+
+    public DataSetup(PostRepository postRepo, UserRepository userRepo,
+                     RoleRepository roleRepository, UserRepository userRepository,
+                     PasswordEncoder passwordEncoder) {
+
+        this.postRepo = postRepo;
+        this.userRepo = userRepo;
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Role userRole = new Role();
+        userRole.setName(ERole.ROLE_USER);
+
+        roleRepository.save(userRole);
+
+        User user1 = new User("user", "user@example.com", passwordEncoder.encode("test"),"Im an activist who donates blood");
+        User user2 = new User("user1", "user1@example.com", passwordEncoder.encode("test"), "I want to kill myself");
+        user1.addRole(userRole);
+        user2.addRole(userRole);
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        postRepo.save(new Post("I hate everyone",user1));
+        postRepo.save(new Post("I love everyone", user2));
+        postRepo.save(new Post("I want to die", user1));
+        postRepo.save(new Post("I want to cry", user2));
+        postRepo.save(new Post("I think earth is flat", user1));
+        postRepo.save(new Post("Whatever", user2));
+        postRepo.save(new Post("Test", user1));
 
 
     }
